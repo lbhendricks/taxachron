9e7ff4ff424eb644d8986fd95ff08975bbf0a199
Lauren Hendricks <laurenbhendricks@gmail.com>
2018-05-16 14:32:03 -0500

    Testing longevity tables code for compatibility with API

--- taxachron_app.R
+++ taxachron_app.R
@@ -1,4 +1,6 @@
 library(shiny)
+library(dplyr)
+library(ggplot2)
 
 #reads time interval names, ages, colors
 time_int<-read.csv("http://paleobiodb.org/data1.1/intervals/list.txt?scale=1&limit=all")
@@ -18,69 +20,41 @@ shinyServer(function(input, output) {
      })
      
      
-     output$plot<-renderPlot({
-          dataset<-dataInput()
-          hist_output<-hist(dataset$mean_age,breaks=seq(0,545,by=1),plot=F)
-          hist_data<-data.frame(counts=hist_output$counts,mids=hist_output$mids)
-          hist_data<-subset(hist_data,hist_data$counts>0)
-          layout(matrix(c(1,2),ncol=2),widths=c(1,5))
-          par(mar=c(5,4,1.25,0))
-          plot(0,0,type="n",xlim=c(0,5),xaxt="n",xlab="",ylab="Age (Ma)",ylim=c(max(hist_data$mids),min(hist_data$mids)),bty="n")
-          rect(0,periods$early_age,5,periods$late_age,col=paste(periods$color))
-          text(rep(2.5,nrow(periods)),rowMeans(cbind(periods$early_age,periods$late_age)),periods$abbrev)
-          mtext(input$taxon,side=3,adj=0,cex=1.5)
-          par(mar=c(5,0,1.25,1))
-          plot(hist_data$counts,hist_data$mids,type="n",xlab="Number of occurrences",xlim=c(0,max(hist_data$counts)),ylim=c(max(hist_data$mids),min(hist_data$mids)),yaxt="n",ylab="",bty="n")
-          segments(rep(0,length(hist_data$mids)),hist_data$mids,hist_data$counts,hist_data$mids,lwd=3,col="steelblue3")
-     })
+     x <- list(canidae,felidae) #through each taxonomic group into a list
      
-     output$downloadData <- downloadHandler(
+     ####  GENERATE LONGEVITY TABLES ####
+     GenerateLongevityTable <- function(x){ #where x is a matrix of pbdb occurrence data fetched via pbdb_occurrences
           
-          filename = function() {
-               paste(input$taxon, ".csv", sep = "")
-          },
+          x <- as.data.frame(x)
+          genera<-x$gnl #What are all genus occurrences for this taxonomic group?
+          genus.names<-unique(genera) #What are the specific genus names?
+          num.genera<-length(genus.names) #How many genera are there? (will be number of rows in the results matrix)
+          outTable<-matrix(NA,nrow=num.genera,ncol=5)
+          colnames(outTable)<-c("FAD","LAD","Duration","Family", "Order")
+          rownames(outTable)<-genus.names
           
-          content = function(file) {
-               occurrences<-dataInput()
-               write.csv(occurrences,file,row.names = FALSE)
+          for(i in 1:nrow(x)){
+               
+               this.taxon <- x$gnl[i] #Define the taxa
+               all.occ.this.taxon <- filter(x, gnl == this.taxon) #Select all rows of that taxon
+               FAD <- max(all.occ.this.taxon$eag, na.rm = T) #Find the max value (FAD) of that taxon...
+               outTable[this.taxon,1] <- FAD #...and insert it into column 1 (FAD)
+               LAD <- min(all.occ.this.taxon$lag, na.rm = T) #Find the min value (LAD) of that taxon...
+               outTable[this.taxon,2] <- LAD #...and insert it into column 2
+               longevity <- FAD - LAD #Calulate the species longevity...
+               outTable[this.taxon,3] <- longevity #...and insert it into column 3
+               Family <- all.occ.this.taxon$fml[1] #Find the family that genus belongs to (the first one in case two are assigned)
+               outTable[this.taxon,4] <- as.character(Family) #...and insert the Family name into column 4
+               Order <- all.occ.this.taxon$odl[1] #Find the order that genus belongs to (the first one in case two are assigned)
+               outTable[this.taxon,5] <- as.character(Order) #...and insert the Order name into column 5
+               outTable<-outTable
+               
           }
-     )
+          
+          df <- as.data.frame(outTable) #Save Result to list
+          
+     }
      
-     
-     maxOcc<-reactive({
-          dataset<-dataInput()
-          subset(dataset,dataset$mean_age==max(dataset$mean_age))
-     })
-     
-     maxOccSp<-reactive({
-          dataset<-dataInput()
-          species<-subset(dataset,dataset$matched_rank==3)
-          species<-subset(species,is.na(species$genus_reso)==T | species$genus_reso=="n. gen.")
-          subset(species,species$mean_age==max(species$mean_age))
-     })
-     
-     output$table1<-renderTable({
-          max_occ<-maxOcc()
-          data.frame(taxon=max_occ$matched_name,
-                     collection=max_occ$collection_no,
-                     early_interval=max_occ$early_interval,
-                     max_age=max_occ$early_age,
-                     late_interval=max_occ$late_interval,
-                     min_age=max_occ$late_age
-          )
-     })
-     
-     
-     output$table2<-renderTable({
-          max_occ<-maxOccSp()
-          data.frame(taxon=max_occ$matched_name,
-                     collection=max_occ$collection_no,
-                     early_interval=max_occ$early_interval,
-                     max_age=max_occ$early_age,
-                     late_interval=max_occ$late_interval,
-                     min_age=max_occ$late_age
-          )
-     })  
 })
 )
 
