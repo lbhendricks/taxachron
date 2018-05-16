ac06f078caaea3fe5c82b7f4b7eb1d8d45a43db8
Lauren Hendricks <laurenbhendricks@gmail.com>
2018-05-16 14:46:37 -0500

    first try at getting code to work with API data

--- LongevityTables_Test.R
+++ LongevityTables_Test.R
@@ -18,24 +18,26 @@ GenerateLongevityTable <- function(x){ #where x is a matrix of pbdb occurrence d
      genera<-x$taxon #What are all genus occurrences for this taxonomic group?
      genus.names<-unique(genera) #What are the specific genus names?
      num.genera<-length(genus.names) #How many genera are there? (will be number of rows in the results matrix)
-     outTable<-matrix(NA,nrow=num.genera,ncol=5)
-     colnames(outTable)<-c("FAD","LAD","Duration","Family", "Order")
+     outTable<-matrix(NA,nrow=num.genera,ncol=3)  # changed to 3 because we don't need family and order anymore
+     colnames(outTable)<-c("FAD","LAD","Duration")#,"Family", "Order")
      rownames(outTable)<-genus.names
      
      for(i in 1:nrow(x)){
           
           this.taxon <- x$taxon[i] #Define the taxa
-          all.occ.this.taxon <- filter(x, taxon == this.taxon) #Select all rows of that taxon
-          FAD <- max(all.occ.this.taxon$eag, na.rm = T) #Find the max value (FAD) of that taxon...
+          all.occ.this.taxon <- dplyr::filter(x, taxon == this.taxon) #Select all rows of that taxon
+          FAD <- max(all.occ.this.taxon$max_age, na.rm = T) #Find the max value (FAD) of that taxon...
           outTable[this.taxon,1] <- FAD #...and insert it into column 1 (FAD)
-          LAD <- min(all.occ.this.taxon$lag, na.rm = T) #Find the min value (LAD) of that taxon...
+          LAD <- min(all.occ.this.taxon$min_age, na.rm = T) #Find the min value (LAD) of that taxon...
           outTable[this.taxon,2] <- LAD #...and insert it into column 2
           longevity <- FAD - LAD #Calulate the species longevity...
           outTable[this.taxon,3] <- longevity #...and insert it into column 3
-          Family <- all.occ.this.taxon$fml[1] #Find the family that genus belongs to (the first one in case two are assigned)
-          outTable[this.taxon,4] <- as.character(Family) #...and insert the Family name into column 4
-          Order <- all.occ.this.taxon$odl[1] #Find the order that genus belongs to (the first one in case two are assigned)
-          outTable[this.taxon,5] <- as.character(Order) #...and insert the Order name into column 5
+          # don't need this because it should all be the same family in the initial version of the API
+          #Family <- all.occ.this.taxon$fml[1] #Find the family that genus belongs to (the first one in case two are assigned)
+          #outTable[this.taxon,4] <- as.character(Family) #...and insert the Family name into column 4
+          # don't need this because it should all be the same order in the initial version of the API
+          #Order <- all.occ.this.taxon$odl[1] #Find the order that genus belongs to (the first one in case two are assigned)
+          #outTable[this.taxon,5] <- as.character(Order) #...and insert the Order name into column 5
           outTable<-outTable
           
      }
@@ -44,6 +46,7 @@ GenerateLongevityTable <- function(x){ #where x is a matrix of pbdb occurrence d
      
 }
 
-LongevityTables <- lapply(x, GenerateLongevityTable)
-names(LongevityTables) <- paste(input,"longevities")
-list2env(LongevityTables, envir = .GlobalEnv)
+# dont't need these because we only have one API request  
+# LongevityTables <- lapply(x, GenerateLongevityTable)
+# names(LongevityTables) <- paste(input,"longevities")
+# list2env(LongevityTables, envir = .GlobalEnv)
