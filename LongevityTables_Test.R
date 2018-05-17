93e92e838e3c2b476813b79120dbfc387f6ff7ec
MatthewBolton <39319846+MatthewBolton@users.noreply.github.com>
2018-05-17 10:02:47 -0500

    Reuploading Greg's changes

--- LongevityTables_Test.R
+++ LongevityTables_Test.R
@@ -1,61 +1,65 @@
 # code from Demo Derby.R
-# testing if it works with the ELC API instead of the pbdb package
+# testing if it works with the ELC API instead of the pbdb packagelibrary(RColorBrewer) #to make pretty colors
+library(ggplot2) #for plottinglibrary(RColorBrewer) #to make pretty colors
+library(dplyr) #for plotting
 
 # decide what genus/family to get info about
-input<-"ursidae"
+input<-"felidae"
 
 # download the data using the ELC API
 occurrences<-read.csv(paste("http://earthlifeconsortium.org/api_v1/occ?taxon=",input,"&ageunits=Ka&output=csv",sep=""))
 
 # not necessary when we have only one taxa that we're showing
-#x <- list(occurrences) # each taxonomic group into a list
-x<-occurrences
+x <- list(occurrences) # each taxonomic group into a list
 
 ####  function to GENERATE LONGEVITY TABLES ####
-GenerateLongevityTable <- function(x){ #where x is a matrix of pbdb occurrence data fetched via pbdb_occurrences
-      #x <- as.data.frame(x)  # turn x into a data frame (holdover from version where multiple families are included)
+GenerateLongevities <- function(x){ #where x is a matrix of pbdb occurrence data fetched via pbdb_occurrences
+  
+      x <- as.data.frame(x)  # turn x into a data frame (holdover from version where multiple families are included)
       x<-x[!is.na(x$min_age),] # remove rows that have a NA for either min age or max age
       x<-x[!is.na(x$max_age),] # remove rows that have a NA for either min age or max age
       
       taxon_split<-strsplit(as.character(x$taxon)," ")  # split the taxon name on spaces
-      genera<-NA       
+      genera<-NA 
+      
       for(row in 1:length(taxon_split)) {
           genera[row]<-taxon_split[[row]][1]
-          }
+      }
+      
       x$genera<-genera  # add the genus column to the data frame
-      #genera<-as.character(x$taxon) #What are all occurrences for this taxonomic group?
       
       genus.names<-unique(genera) #What are the unique genus names?
       num.genera<-length(genus.names) #How many genera are there? (will be number of rows in the results matrix)
       
-      outTable<-data.frame(matrix(NA,nrow=num.genera,ncol=4))  # ncol=5 changed to 3 because we don't need family and order anymore
-      colnames(outTable)<-c("Taxa","FAD","LAD","Duration")#,"Family", "Order")
-      #rownames(outTable)<-genus.names
+      outTable<-data.frame(matrix(NA,nrow=num.genera,ncol=4))  
+      colnames(outTable)<-c("Taxa","FAD","LAD","Duration")
       outTable$Taxa<-genus.names
       
       for(species in 1:nrow(outTable)){
           this.taxon <- outTable[species,1] #Define the taxa
           all.occ.this.taxon <- dplyr::filter(x, x$genera == this.taxon) #Select all rows of that taxon
           FAD <- max(all.occ.this.taxon$max_age, na.rm = T) #Find the max value (FAD) of that taxon...
-          outTable[species,2] <- FAD #...and insert it into column 1 (FAD)
+          outTable[species,2] <- FAD #...and insert it into column 2 (FAD)
           LAD <- min(all.occ.this.taxon$min_age, na.rm = T) #Find the min value (LAD) of that taxon...
-          outTable[species,3] <- LAD #...and insert it into column 2
+          outTable[species,3] <- LAD #...and insert it into column 3
           longevity <- FAD - LAD #Calulate the species longevity...
-          outTable[species,4] <- longevity #...and insert it into column 3
-          # don't need this because it should all be the same family in the initial version of the API
-          #Family <- all.occ.this.taxon$fml[1] #Find the family that genus belongs to (the first one in case two are assigned)
-          #outTable[this.taxon,4] <- as.character(Family) #...and insert the Family name into column 4
-          # don't need this because it should all be the same order in the initial version of the API
-          #Order <- all.occ.this.taxon$odl[1] #Find the order that genus belongs to (the first one in case two are assigned)
-          #outTable[this.taxon,5] <- as.character(Order) #...and insert the Order name into column 5
+          outTable[species,4] <- longevity #...and insert it into column 4
           outTable<-outTable
           }
+
 df <- as.data.frame(outTable) #Save Result to list
-}
 
-LongevityTable<-GenerateLongevityTable(x)
+# Plot the longevities
+g <- ggplot(df, aes(x = Longevity, y = Taxa)) +
+  geom_segment(aes(x = as.numeric(as.character(LAD)), y = Taxa, xend = as.numeric(as.character(FAD)), yend = Taxa)) +
+  geom_text(aes(x = as.numeric(as.character(FAD)), y = Taxa, label = Taxa), nudge_x = 1, nudge_y = 1) +
+  theme(axis.text.y=element_blank(),
+        axis.ticks.y=element_blank()) +
+  ggtitle("Longevities")
+g
 
-# dont't need these because we only have one API request  
-# LongevityTables <- lapply(x, GenerateLongevityTable)
-# names(LongevityTables) <- paste(input,"longevities")
-# list2env(LongevityTables, envir = .GlobalEnv)
+} #end function
+
+Longevities <- lapply(x, GenerateLongevities)
+
+Longevities
